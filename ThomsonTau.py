4779fe415f2f06b82f496c32b16dd04ac6e41739
BlancaNietoPetinal <blancanietopetinal@gmail.com>
2019-07-28 22:22:35 +0200

    Update ThomsonTau.py

--- ThomsonTau.py
+++ ThomsonTau.py
@@ -9,21 +9,26 @@ def maxim(data):
 		if not(np.isnan(data[i])) and data[i]>m:
 			m = data[i]
 			ind = i
-			print('new max in:',i,'which is:',m)
 	return m,ind
 
-def thompson_tau(data,alpha,threshold):
-	# https://www.statisticshowto.datasciencecentral.com/modified-thompson-tau-test/
-	# return a list with the outliers index
+def thompson_tau(data,alpha = 0.05,threshold):
+	# Information obtained from: https://www.statisticshowto.datasciencecentral.com/modified-thompson-tau-test/
+	'''
+	Implements the Thompson Tau method and returns a list with the outliers index.
+	Inputs:
+		- data: an array.
+		- alpha: the significance level, default 0.05.
+		- Threshold: the number of points tested.
+	Outputs:
+	 	- outliers: a list with the indices of the outliers.
+	'''
 	outliers = []
 	n = len(data)
 	mean = np.mean(data)
 	delta = abs(data-mean)
 	std = np.std(data)
 	for i in range(threshold):
-		print(delta)
 		d,ind = maxim(delta)
-		print(d, ind)
 		reject = ss.t.ppf(alpha/2, n - 2)
 		tau = (reject*(n - 1))/(np.sqrt(n)*np.sqrt(n - 2 + np.power(reject,2)))
 		if d > tau*std:
@@ -31,5 +36,6 @@ def thompson_tau(data,alpha,threshold):
 		delta[ind] = None
 	return outliers
 
+# An example to test it
 data = np.array([489, 490, 490, 491, 494, 499, 499, 500, 501, 505])
 print(thompson_tau(data,0.05,10))
